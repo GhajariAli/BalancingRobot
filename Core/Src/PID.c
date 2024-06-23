e687d407cbf0fe518debca00d202c8a7368fa2dd
Salman Ami Motlaq <66294582+SMotlaq@users.noreply.github.com>
2024-06-23 03:07:57 -0700

    fix: integral anti-windup limits edited

--- Core/Src/PID.c
+++ Core/Src/PID.c
@@ -8,10 +8,10 @@ void updatePID(PID_Controller* pid, double current) {
 	D = pid->Kd * (pid->CurrentError - pid->prev_error) / (pid->dt/1000);
 	pid->output = P + pid->integral + D;
 	// Integral with anti-windup
-	if (pid->integral > pid->max_output) {
-		pid->integral = pid->max_output;
-	} else if (pid->integral < pid->min_output) {
-		pid->integral = pid->min_output;
+	if (pid->integral > pid->max_Integral) {
+		pid->integral = pid->max_Integral;
+	} else if (pid->integral < pid->min_Integral) {
+		pid->integral = pid->min_Integral;
 	}
 	// Saturate output within limits
 	if (pid->output > pid->max_output) {
